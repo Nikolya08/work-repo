798d490155d749f97cace386b979917aa460ea00
MykolaSemenyk <kolya17364654@gmail.com>
2020-04-04 22:02:03 +0300

    UserPage

--- user-page/src/app/constants/api.ts
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = 'v1/user';
-export const TOKEN_KEY = 'token';
-export const USER_KEY = 'user';
-export const EMAIL_KEY = 'email';
--- user-page/src/app/constants/routing.ts
+++
@@ -1,8 +0,0 @@
-export const ROUTES_STRINGS = {
-  LOGIN: 'login',
-  REGISTER: 'register',
-  MAIN: '',
-  PROFILE: 'profile',
-  MAP: 'map',
-  LIST: 'list'
-};
