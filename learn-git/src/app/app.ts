445c850b4c1ba64ff029621ee722fae339830d4a
Semeniuk Mykola <mykola.semeniuk@suntechinnovation.com>
2025-08-01 11:50:20 +0300

    Add first component to app

--- learn-git/src/app/app.ts
+++ learn-git/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { First } from './componets/first/first';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, First],
   templateUrl: './app.html',
   standalone: true,
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
   protected readonly title = signal('learn-git');
